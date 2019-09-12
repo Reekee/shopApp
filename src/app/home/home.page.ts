5c7d335f09e6ee8684b201ac1694adc3623abf34
สริกีร์ เอียดตรง <srikee.e@psu.ac.th>
2019-09-12 15:53:36 +0700

    save

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,12 +9,26 @@ import { Storage } from '@ionic/storage';
 })
 export class HomePage {
   member = {};
+  product = [];
   constructor(
     private session: SessionService,
     private storage: Storage
   ) {
     this.storage.get('member').then((val) => {
       this.member = val;
+      this.loadData();
+    });
+  }
+  loadData() {
+    this.session.ajax(this.session.api + "product-get.php", {
+    }, true).then((res: any) => {
+      if (res.status == true) {
+        this.product = res.product;
+      } else {
+        this.session.showAlert("เข้าสู่ระบบไม่สำเร็จ"); //alert("เข้าสู่ระบบไม่สำเร็จ");
+      }
+    }).catch(error => {
+      this.session.showAlert(error); //alert(error);
     });
   }
 }
