7532172ac51bc15004a6a0f600a42879fe8d09af
สริกีร์ เอียดตรง <srikee.e@psu.ac.th>
2019-09-12 09:43:38 +0700

    เพิ่ม tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  /*{
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
@@ -10,13 +11,13 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
+  },*/
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
   },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' }
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
 ];
 
 @NgModule({
@@ -25,4 +26,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
